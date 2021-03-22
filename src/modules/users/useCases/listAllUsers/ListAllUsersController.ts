3c216cecf192e7c3923f3f8fa3518eb07b536c93
Patrick Paiva <patrickpaiva88@gmail.com>
2021-03-22 16:12:54 -0300

    added logic to listAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.headers.user_id as string;
+
+    this.listAllUsersUseCase.execute({ user_id });
+
+    return response.status(201).send();
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const findUser = this.usersRepository.findById(user_id);
+
+    if (!findUser.admin) {
+      throw new Error("Access Denied");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
